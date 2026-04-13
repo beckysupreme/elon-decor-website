bb0473f7485091e5631d742cfa12b89e67541a29
beckysupreme <beckygetnet8@gmail.com>
2026-04-13 14:21:45 +0300

    ain

--- server/server.js
+++ server/server.js
@@ -6,9 +6,27 @@ const mongoose = require('mongoose');
 dotenv.config();
 
 const app = express();
+const allowedOrigins = [
+  'https://elon-decor-website.vercel.app',
+  'https://elon-decor.vercel.app',
+  'http://localhost:5173',
+  'http://localhost:3000'
+];
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true ,limit: '50mb' }));
 
